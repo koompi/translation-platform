5167f0b56279934efdac122d31c3517ce025299a
ismesunny <lorshousunny@gmail.com>
2020-10-19 12:39:32 +0700

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello World");
+#[macro_use]
+extern crate diesel;
+
+mod routes;
+mod models;
+mod schema;
+
+use actix_web::{App, HttpServer, web};
+use diesel::r2d2::{self, ConnectionManager};
+use diesel::SqliteConnection;
+
+pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("Database not found");
+    let database_pool = Pool::builder()
+        .build(ConnectionManager::<SqliteConnection>::new(database_url))
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .data(database_pool.clone())
+            .route("/", web::get().to(routes::home))
+            .route("/addlink", web::post().to(routes::add_link))
+            .route("/getlinks", web::get().to(routes::get_links))
+    })
+    .bind("127.0.0.1:8888")?
+    .run()
+    .await
 }
